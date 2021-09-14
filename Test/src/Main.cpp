294168a4820e06232e1fb33341b89c1da73b33a0
AnthoFoxo <anthofoxo@gmail.com>
2021-09-14 14:31:41 -0400

    fixed dist build

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -1,6 +1,14 @@
+#if defined(FE_PLAT_WINDOWS) && defined(FE_CONF_DIST)
+#	include <Windows.h>
+#endif
+
 #include <spdlog/spdlog.h>
 
-int main()
+#if defined(FE_PLAT_WINDOWS) && defined(FE_CONF_DIST)
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR pCmdLine, _In_ int nCmdShow)
+#else
+int main(int argc, char* argv[])
+#endif
 {
 	spdlog::info("OpenGL Testbed");
 }
